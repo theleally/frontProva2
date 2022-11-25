72426817890fd1ca78b21fdd852195f1227f6a1a
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-11-24 23:56:11 -0300

    listar jogos

--- src/app/components/pages/jogo/cadastrar-jogo/cadastrar-jogo.component.ts
+++ src/app/components/pages/jogo/cadastrar-jogo/cadastrar-jogo.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from "@angular/router";
 import { Selecao } from "src/app/models/selecao.model";
 import { Jogo } from "src/app/models/jogo.model";
 import { SelectControlValueAccessor } from "@angular/forms";
-
 
 @Component({
   selector: "app-cadastrar-jogo",
@@ -14,35 +13,44 @@ import { SelectControlValueAccessor } from "@angular/forms";
 })
 export class CadastrarJogoComponent implements OnInit {
   selecoes!: Selecao[];
-  id!: number;
+  selecaoA!: Selecao;
+  selecaoB!: Selecao;
 
-  constructor(private http: HttpClient, private router: Router, private _snackBar: MatSnackBar) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.http.get<Selecao[]>("https://localhost:5001/api/selecao/listar").subscribe({
-      next: (selecoes) => {
-        this.selecoes = selecoes;
-      },
-    });
+    this.http
+      .get<Selecao[]>("https://localhost:5001/api/selecao/listar")
+      .subscribe({
+        next: (selecoes) => {
+          this.selecoes = selecoes;
+        },
+      });
   }
 
   cadastrar(): void {
-    console.log(this.id);
+    console.log(this.selecaoA);
+    console.log(this.selecaoB);
 
     let jogo: Jogo = {
-      
+      //selecaoA: this.selecaoA,
+      //selecaoB: this.selecaoB,
     };
 
-    this.http.post<Jogo>("https://localhost:5001/api/jogo/cadastrar", jogo).subscribe({
-      next: (jogo) => {
-        this._snackBar.open("Jogo cadastrado!", "Ok!", {
-          horizontalPosition: "right",
-          verticalPosition: "top",
-        });
-        this.router.navigate(["pages/jogo/listar"]);
-      },
-    });
+    this.http
+      .post<Jogo>("https://localhost:5001/api/jogo/cadastrar", jogo)
+      .subscribe({
+        next: (jogo) => {
+          this._snackBar.open("Jogo cadastrado!", "Ok!", {
+            horizontalPosition: "right",
+            verticalPosition: "top",
+          });
+          this.router.navigate(["pages/jogo/listar"]);
+        },
+      });
   }
-
-  
 }
